dddda0942bba9c84fed8a4e53c930a204ae5c571
Hans Thinh Le <hans.thinhle@gmail.com>
2017-02-07 17:23:36 +0700

    Update version of the website

--- watch.py
+++ watch.py
@@ -2,7 +2,7 @@ from staticjinja import make_site
 
 
 if __name__ == "__main__":
-    context = {'version': '3'}
+    context = {'version': '4'}
     site = make_site(
         outpath='output',
         extensions=['jinja2.ext.with_',],
