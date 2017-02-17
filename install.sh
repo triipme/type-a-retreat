05008cf53194899830cc055007cf627ff82fb325
coyotevn <trivo@Tris-MacBook-Pro-2.local>
2017-02-17 15:07:58 +0700

    Update install script to allow failure when installing virtualenv

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-pip install virtualenvwrapper
-source $VIRTUALENVWRAPPER_SCRIPT
-mkvirtualenv -p python2 type-a-retreat
+pip install virtualenvwrapper > /dev/null 2>&1 && \
+source $VIRTUALENVWRAPPER_SCRIPT > /dev/null 2>&1 && \
+mkvirtualenv -p python2 type-a-retreat && \
 workon type-a-retreat
 pip install -r requirements.txt
 gem install s3_website
