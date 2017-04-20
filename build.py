324cc831f0062515f1b952ef6b970b7eda40d3f2
Hans Thinh Le <hans.thinhle@gmail.com>
2017-04-20 16:13:30 +0700

    Update new version and style

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from staticjinja import make_site
 
 
 if __name__ == "__main__":
-    context = {'version': '6'}
+    context = {'version': '7'}
     site = make_site(
         outpath='output',
         extensions=['jinja2.ext.with_', ],
