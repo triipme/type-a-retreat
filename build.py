69c270ff6d0282f2cb11c31106f165755d89539e
Hans Thinh Le <hans.thinhle@gmail.com>
2017-05-12 14:47:17 +0700

    Fix breaking line of heading on mobile

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from staticjinja import make_site
 
 
 if __name__ == "__main__":
-    context = {'version': '10'}
+    context = {'version': '11'}
     site = make_site(
         outpath='output',
         extensions=['jinja2.ext.with_', ],
