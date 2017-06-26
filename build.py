e02f26815fe437dc11c2a9ee2dc892e27d353e93
Hans Thinh Le <hans.thinhle@gmail.com>
2017-06-26 19:08:55 +0700

    Add Testimonials section

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ args = parser.parse_args()
 should_watch = args.watch
 
 if __name__ == "__main__":
-    context = {'version': '14'}
+    context = {'version': '15'}
     site = make_site(
         outpath='output',
         extensions=['jinja2.ext.with_', ],
